dfd7685fab4cbb396c23c977efc73f3c334e6d5e
Scotty Weeks <scott.weeks@gmail.com>
2013-12-19 13:04:16 -0500

    added thinking sphinx env

--- spec/indices/spree_product_index_spec.rb
+++ spec/indices/spree_product_index_spec.rb
@@ -1,1 +1,11 @@
 require 'spec_helper'
+require 'support/sphinx_environment'
+
+describe "Spree::Product Index" do
+  sphinx_environment :spree_products do
+    before :each do
+      # index some shit, yo.
+    end
+    it "should index some shit. Yo."
+  end
+end
